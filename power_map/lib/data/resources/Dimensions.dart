dba63efa3cf149b78d85df6acf283eab92686fff
Jesse Scott <jesse.scott@meettally.com>
2020-04-06 16:44:29 -0700

    cleanup

--- power_map/lib/data/resources/Dimensions.dart
+++ power_map/lib/data/resources/Dimensions.dart
@@ -1,7 +1,4 @@
 class Dimensions {
   // Padding
-  static const double small_padding = 12;
   static const double base_padding = 16;
-  static const double large_padding = 32;
-  static const double huge_padding = 64;
 }
--- power_map/lib/domain/navigation/Navigation.dart
+++ power_map/lib/domain/navigation/Navigation.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:powermap/view/screens/Home.dart';
 
 class Navigation {
-  static const String homePage = "/home";
-
   static void goToHome(BuildContext context) {
     Navigator.pushReplacement(
       context,
